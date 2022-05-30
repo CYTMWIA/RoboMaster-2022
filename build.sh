3a3ee9a1931c0883fc0943056ee78b7d759e2210
_______ <8965180@qq.com>
2022-05-30 15:08:52 +0800

    完善编译选项

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd ${SCRIPTPATH}/
 
+echo $(pwd)
+
 mkdir -p build && cd build
 
 ln -s ../asset/* ./
 
-cmake .. && make -j$(($(nproc) + 1))
+cmake -DMODEL_RUNNER=openvino_yolox .. && make -j$(($(nproc) + 1))
 
 if [ $? -eq 0 ] && [ "$1" == "--run" ]; then
     ./RMCV
